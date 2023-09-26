95fdd561904dfd2c55b213eec630146b27ee32ce
Koen Zwikstra <kmz.zwikstra@belastingdienst.nl>
2023-09-26 11:38:38 +0200

    adds iOS popup bug repro app

---
+++ Source/PopupBug/PopupBug/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace PopupBug;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		this.ShowPopup(new MyPopup());
+	}
+}
+
+
---
+++ Source/PopupBug/PopupBug/MyPopup.xaml.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace PopupBug;
+
+public partial class MyPopup : Popup
+{
+	public MyPopup()
+	{
+		InitializeComponent();
+	}
+}
