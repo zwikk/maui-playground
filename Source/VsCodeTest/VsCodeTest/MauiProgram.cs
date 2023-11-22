ec6574e8ded9cde3a710aa3180a943cf8ad4d2a4
Koen Zwikstra <kmz.zwikstra@belastingdienst.nl>
2023-11-22 12:03:29 +0100

    adds VsCodeTest proj

---
+++ Source/VsCodeTest/VsCodeTest/MauiProgram.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.Extensions.Logging;
+
+namespace VsCodeTest;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		//_ = TestAnalyticsAsync();
+
+		return builder.Build();
+	}
+
+	static async Task TestAnalyticsAsync()
+	{
+		Analytics.StartSession();
+		Analytics.TrackEvent("Hello world", null);
+
+		if (await Analytics.IsEnabledAsync())
+		{
+			Console.WriteLine("Analytics enabled");
+		}
+		else
+		{
+			Console.WriteLine("Analytics not enabled");
+		}
+	}
+}
